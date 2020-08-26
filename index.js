80142f649f9ece4fcb02831d40d7f87a8d4f60af
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-08-25 22:57:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file
+
+// array = ['Dan', 'Daniel', 'Danielle', 'Monica', 'McDaniel']
+
+const findMatching = function(array, name) {
+    return array.filter(function(driverName) {
+       return driverName.toUpperCase() === name.toUpperCase();
+    });
+}
+
+const fuzzyMatch = function(array,letters) {
+    let letterCount = letters.length;
+    return array.filter(function(driver){
+       return driver.slice(0,letterCount) === letters;
+    });
+}
+
+const matchName = function(array, string) {
+    return array.filter(function(driver){
+        return driver.name === string;
+    });
+}
+
